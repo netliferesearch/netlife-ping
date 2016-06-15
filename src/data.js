2f9bee5c32e939b7c0d94f1fdd68b63bee287874
Fredrik Jensen <fredrik@netliferesearch.com>
2016-06-15 23:44:34 +0200

    Cleaned up Google fetching a bit

--- src/data.js
+++ src/data.js
@@ -3,6 +3,8 @@
 const SpreadSheet = require('google-spreadsheet-reader');
 const spreadSheet = new SpreadSheet('1geItk1iXyELsMgb76AH0Ze14juGdEu4mpx6RGjRmzCM');
 
-module.exports = spreadSheet.load()
+const loadData = spreadSheet.load()
     .then(res => res)
     .catch(err => { console.error(err.message); });
+
+module.exports = loadData;
