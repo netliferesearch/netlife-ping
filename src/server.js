9d1346e0e367de4751e1bfc6aae39f1adf2965db
kmelve <knut@netliferesearch.com>
2016-06-15 08:46:17 +0200

    monitor more pins (because the Bergen board pin 2 is F-ed), send message as argument from server

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ const board = new five.Board();
 const bot = require('./bot');
 
 board.on('ready', () => {
-    const button = new five.Button(2);
+    const buttons = new five.Buttons([2, 3, 4]);
+    const message = "Heisann, du har besøk!";
 
-    button.on('release', () => {
-        bot.sendMessage();
+    buttons.on('release', () => {
+        bot.sendMessage('@bergen', message);
+    ***REMOVED***
+
+    const users = {
+        button1: { pin: 3, value: '@dataknut' },
+        button2: { pin: 4, value: '@netlife-ping' },
     ***REMOVED***
 ***REMOVED***
