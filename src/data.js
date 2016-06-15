e7f861b780e11d1397d26364863be26644b11bbf
Fredrik Jensen <fredrik@netliferesearch.com>
2016-06-15 23:30:18 +0200

    Added data from Google Spreadsheet

---
+++ src/data.js
@@ -0,0 +1,8 @@
+***REMOVED***
+
+const SpreadSheet = require('google-spreadsheet-reader');
+const spreadSheet = new SpreadSheet('1geItk1iXyELsMgb76AH0Ze14juGdEu4mpx6RGjRmzCM');
+
+module.exports = spreadSheet.load()
+    .then(res => res)
+    .catch(err => { console.error(err.message); ***REMOVED***
