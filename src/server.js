8ff6af51574d4095620160a6c979597846ff5795
Fredrik Jensen <fredrik@netliferesearch.com>
2016-06-15 23:30:18 +0200

    Added data from Google Spreadsheet

--- src/server.js
+++ src/server.js
@@ -3,17 +3,27 @@
 const five = require('johnny-five');
 const board = new five.Board();
 const bot = require('./bot');
+const users = require('./data');
+
+/* Data from Google Spreadsheet ?
+https://docs.google.com/spreadsheets/d/1geItk1iXyELsMgb76AH0Ze14juGdEu4mpx6RGjRmzCM/edit#gid=0
+Haven´t tested if it actually works :) */
+let usermap = {};
+
+users.then(res => {
+    usermap = res;
+    console.log(usermap);
+});
 
 board.on('ready', () => {
     /* @todo This doesn't scale, we should find a way to serialize the stuff*/
     const buttons = new five.Buttons([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     const defaultMessage = 'Noen trykka på knappen (igjen)!';
 
-    /* @todo Move to CONTENTFUL or something? */
-    const usermap = [
+    /* const usermap = [
         { pin: 3, handle: '#netlife-ping', message: 'trykka på den ene knappen' },
         { pin: 4, handle: '#netlife-ping', message: 'trykka på den andre knappen' },
-    ];
+    ]; */
 
     buttons.on('release', (e) => {
         const pin = e.pin;
