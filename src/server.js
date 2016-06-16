0128ffc5ced5acfe697688c5839db0e08d81d68d
kmelve <knut@netliferesearch.com>
2016-06-16 07:03:49 +0200

    use google spreadsheet for mapping, wait for users to load before initiating the board

--- src/server.js
+++ src/server.js
@@ -3,33 +3,21 @@
 const five = require('johnny-five');
 const board = new five.Board();
 const bot = require('./bot');
-const users = require('./data');
+const userData = require('./data');
 
-/* Data from Google Spreadsheet ?
-https://docs.google.com/spreadsheets/d/1geItk1iXyELsMgb76AH0Ze14juGdEu4mpx6RGjRmzCM/edit#gid=0
-Haven´t tested if it actually works :) */
-let usermap = {};
+const usermap = () => userData.then(res => res[1]);
 
-users.then(res => {
-    usermap = res;
-    console.log(usermap);
-});
+usermap().then(users => {
+    board.on('ready', () => {
 
-board.on('ready', () => {
-    /* @todo This doesn't scale, we should find a way to serialize the stuff*/
-    const buttons = new five.Buttons([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    const defaultMessage = 'Noen trykka på knappen (igjen)!';
+        /* @todo This doesn't scale, we should find a way to serialize the stuff*/
+        const buttons = new five.Buttons([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-    /* const usermap = [
-        { pin: 3, handle: '#netlife-ping', message: 'trykka på den ene knappen' },
-        { pin: 4, handle: '#netlife-ping', message: 'trykka på den andre knappen' },
-    ]; */
-
-    buttons.on('release', (e) => {
-        const pin = e.pin;
-        usermap.filter(u => u.pin === pin).map(u => bot.sendMessage(u.handle, u.message));
+        buttons.on('release', (e) => {
+            users.filter(u => u.pin === e.pin).map(u => bot.sendMessage(u.handle, u.message));
+        });
+        buttons.on('hold', () => {
+            bot.sendMessage('#netlife-ping', 'noen holder på knappen');
+        });
     });
-    buttons.on('hold', () => {
-        bot.sendMessage('#netlife-ping', 'noen holder på knappen');
-    });
-});
+}).catch(err => console.log(err));
